4fb00f465f0acf9eeaacbef77ec0718fa1b61b8f
Nick Valdez <nicholastvaldez@gmail.com>
2023-03-01 13:21:22 -0600

    checkout to main to rename branch

--- unique_invoice_countries.sql
+++
@@ -1,6 +0,0 @@
-SELECT e.FirstName || ' ' || e.LastName AS FullName,
-       i.InvoiceId
-FROM employee e
-JOIN customer c ON e.EmployeeId = c.SupportRepId
-JOIN invoice i ON c.CustomerId = i.CustomerId
-WHERE e.Title = 'Sales Support Agent';
